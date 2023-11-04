5571320c0bc3bf59b08e019cc9c08185b7dbb31a
mdjamee306 <mdjamee400@gmail.com>
2023-11-05 05:00:22 +0600

    cart add product and cart delete product done

--- src/Pages/AddCart.jsx
+++ src/Pages/AddCart.jsx
@@ -1,30 +1,66 @@
-import React from 'react';
+// /* eslint-disable react/prop-types */
+// import { useState } from 'react';
+// import Swal from 'sweetalert2';
 
-const AddCart = ({ data }) => {
-    const { name, _id, brand, type, description, price, rating, image } = data;
-    return (
-        <div className='md:my-8 my-4'>
-            <div className="cursor-pointer relative flex flex-col shadow-2xl rounded-xl bg-clip-border bg-slate-300 hover:bg-gray-500">
-            <div className="relative mx-4 mt-4 overflow-hidden bg-white rounded-xl bg-clip-border">
-                <img
-                    src={image}
-                    className="object-contain w-11/12 h-60 mx-auto "
-                />
-            </div>
-            <div className="p-6 space-y-2">
-                <h5 className="text-xl text-black">Brand: <span className="text-sm font-extrabold text-black">{brand}</span></h5>
-                <span className="block font-sans text-xl antialiased font-extrabold text-purple-900">{name}</span>
-                
-                <h3 className="text-black p-1 rounded-full border-black border-2 text-center">Rating: <span>{rating}</span></h3>
-                <p className="text-xs h-[100px]">{description}</p>
-                <p className="text-xl text-black">Price: $<span className="text-sm font-extrabold text-black">{price}</span></p>
-            </div>
-            <div className='text-center'>
-                <button className='btn'>Delete</button>
-            </div>
-        </div>
-        </div>
-    );
-};
+// const AddCart = ({ datas }) => {
 
-export default AddCart;
+//     // Use 'datas' as the initial state of 'product'
+//     const [product, setProduct] = useState(datas);
+
+//     const handleDelete = (_id) => {
+//         console.log("delete", _id);
+//         fetch(`http://localhost:5000/cart/${_id}`, {
+//             method: "DELETE",
+//         })
+//             .then((res) => res.json())
+//             .then((data) => {
+//                 console.log(data);
+//                 if (data.deletedCount > 0) {
+//                     Swal.fire({
+//                         title: 'Deleted!',
+//                         text: 'Your product has been deleted.',
+//                         icon: 'success',
+//                     });
+
+//                     // Remove the deleted product from the state
+//                     const remaining = product.filter((item) => item._id !== _id);
+//                     setProduct(remaining);
+//                 } else {
+//                     Swal.fire({
+//                         title: 'Error',
+//                         text: 'Failed to delete the product.',
+//                         icon: 'error',
+//                     });
+//                 }
+//             })
+//             .catch((error) => {
+//                 console.error('Error deleting product:', error);
+//             });
+//     };
+//     return (
+//         <div className='md:my-8 my-4'>
+//             <div className="cursor-pointer relative flex flex-col shadow-2xl rounded-xl bg-clip-border bg-slate-300 hover:bg-gray-500">
+//                 <div className="relative mx-4 mt-4 overflow-hidden bg-white rounded-xl bg-clip-border">
+//                     <img
+//                         src={product.image}
+//                         className="object-contain w-11/12 h-60 mx-auto "
+//                     />
+//                 </div>
+//                 <div className="p-6 space-y-2">
+//                     <h5 className="text-xl text-black">Brand: <span className="text-sm font-extrabold text-black">{product.brand}</span></h5>
+//                     <span className="block font-sans text-xl antialiased font-extrabold text-purple-900">{product.name}</span>
+
+//                     <h3 className="text-black p-1 rounded-full border-black border-2 text-center">Rating: <span>{product.rating}</span></h3>
+//                     <p className="text-xs h-[100px]">{product.description}</p>
+//                     <p className="text-xl text-black">Price: $<span className="text-sm font-extrabold text-black">{product.price}</span></p>
+//                 </div>
+//                 <div className='text-center my-4'>
+//                     <button onClick={() => handleDelete(product._id)} className='btn w-11/12 mx-auto hover:bg-slate-300
+//                 hover:text-black'>Delete</button>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default AddCart;
--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,14 +1,71 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
-import AddCart from "./AddCart";
+import Swal from "sweetalert2";
 
 const Cart = () => {
-    const product = useLoaderData();
-    console.log(product);
+    const loadProduct = useLoaderData();
+    // console.log(product);
+    // Use 'datas' as the initial state of 'product'
+    const [product, setProduct] = useState(loadProduct);
+
+    const handleDelete = (_id) => {
+        console.log("delete", _id);
+        fetch(`http://localhost:5000/cart/${_id}`, {
+            method: "DELETE",
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                if (data.deletedCount > 0) {
+                    Swal.fire({
+                        title: 'Deleted!',
+                        text: 'Your product has been deleted.',
+                        icon: 'success',
+                    });
+
+                    // Remove the deleted product from the state
+                    const remaining = product.filter((item) => item._id !== _id);
+                    setProduct(remaining);
+                } else {
+                    Swal.fire({
+                        title: 'Error',
+                        text: 'Failed to delete the product.',
+                        icon: 'error',
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error('Error deleting product:', error);
+            });
+    };
     return (
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-3/4 mx-auto">
-            
+
             {
-                product?.map(data =><AddCart key={data._id} data={data}></AddCart>)
+                product?.map(datas => <div key={datas._id} className='md:my-8 my-4'>
+                    <div className="cursor-pointer relative flex flex-col shadow-2xl rounded-xl bg-clip-border bg-slate-300 hover:bg-gray-500">
+                        <div className="relative mx-4 mt-4 overflow-hidden bg-white rounded-xl bg-clip-border">
+                            <img
+                                src={datas.image}
+                                className="object-contain w-11/12 h-60 mx-auto "
+                            />
+                        </div>
+                        <div className="p-6 space-y-2">
+                            <h5 className="text-xl text-black">Brand: <span className="text-sm font-extrabold text-black">{datas.brand}</span></h5>
+                            <span className="block font-sans text-xl antialiased font-extrabold text-purple-900">{datas.name}</span>
+
+                            <h3 className="text-black p-1 rounded-full border-black border-2 text-center">Rating: <span>{datas.rating}</span></h3>
+                            <p className="text-xs h-[100px]">{datas.description}</p>
+                            <p className="text-xl text-black">Price: $<span className="text-sm font-extrabold text-black">{datas.price}</span></p>
+                        </div>
+                        <div className='text-center my-4'>
+                            <button onClick={() => handleDelete(datas._id)} className='btn w-11/12 mx-auto hover:bg-slate-300
+                    hover:text-black'>Delete</button>
+                        </div>
+                    </div>
+                </div>)
+
             }
 
         </div>
